5f5994213924df44d5073fdd3788cb644152f1c2
Gokhan841 <56187596+Gokhan841@users.noreply.github.com>
2019-12-26 17:26:38 +0300

    Create Personel.cs

---
+++ Personel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class Personel
+    {
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public DateTime tarih { get; set; }
+        public int tc { get; set; }
+
+        public void PersonelEkle()
+        {
+
+        }
+    }
+}
